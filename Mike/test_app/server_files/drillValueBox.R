6cc6516a39c5b02c82ee5996f22a84c44e35e121
mtseman <mtseman@gamil.com>
2018-02-20 00:26:29 -0500

    comments

--- Mike/test_app/server_files/drillValueBox.R
+++ Mike/test_app/server_files/drillValueBox.R
@@ -1,5 +1,10 @@
 
-
+#---------------------------
+# UI infoBoxes
+#---------------------------
+#
+# What I did was wrap the standard infoBox function with an additonal div that there allows for modified css 
+# (pointer style cursor) and js on click function. For each invo box, specify what dataset you wnat in the modal as an arguement to the onclick function
 output$testInfoBox_mtcars<-renderUI({
 
   #turn this into a function - paramters include what dataset you want to pull up in modal
@@ -20,30 +25,21 @@ output$testInfoBox_iris<-renderUI({
 
 
 
-
+#------------------------------
+# ObsereEvent and Modal Function
+#-----------------------------
 #Watches for change on input$shinydrill which is just a random number generated onclick event see app.js drilltest() function
 observeEvent(input$shinydrill,{
   
- 
   #also echos back what dataset we want to show in the modal
   #input$shinydrill_dataset is the dataset we want to pull based on what infoBox we clicked
   
-    showModal(dataModal_InfoBox(title=paste('Complete data set for ',input$shinydrill_dataset,sep='')))
-    
+  showModal(dataModal_InfoBox(title=paste('Complete data set for ',input$shinydrill_dataset,sep='')))
     
   
 })
 
-#Dynamic Data Table for info boxes - listens to input$shinydrill_dataset to pick the data for the modal
-output$testInfoBox_DT<-DT::renderDataTable({
-  
-  if (input$shinydrill_dataset=='mtcars')(df<-mtcars)
-  if (input$shinydrill_dataset=='iris')(df<-iris)
-  
-  datatable(df)
-  
-  
-})
+
 
 dataModal_InfoBox<-function(title='Complete Dataset for mtcars'){
   
@@ -55,4 +51,18 @@ dataModal_InfoBox<-function(title='Complete Dataset for mtcars'){
   
     )
 }
+
+#-------------------------------------------
+# DT for Modal
+#--------------------------------------------
+#Dynamic Data Table for info boxes - listens to input$shinydrill_dataset to pick the data for the modal
+output$testInfoBox_DT<-DT::renderDataTable({
+  
+  if (input$shinydrill_dataset=='mtcars')(df<-mtcars)
+  if (input$shinydrill_dataset=='iris')(df<-iris)
+  
+  datatable(df)
+  
+  
+})
 
