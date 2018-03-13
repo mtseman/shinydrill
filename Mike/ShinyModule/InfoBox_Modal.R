f1fc032e5164b105c7bddd317aa0d64adc2d740c
mtseman <mtseman@gamil.com>
2018-03-13 12:09:54 -0400

    some missing arguments in function calls for this example. My global environment was preventing me from seeing recent changes to the package. Clear invironment and now MOdal function reflects what is on git

--- Mike/ShinyModule/InfoBox_Modal.R
+++ Mike/ShinyModule/InfoBox_Modal.R
@@ -3,8 +3,8 @@ library(shinyjs)
 library(shinydashboard)
 library(DT)
 library(devtools)
-install_github('mtseman/shinyModals')
-
+#install_github('mtseman/shinyModals',force=T)
+library(shinyModals)
 
 
 ui<-dashboardPage(
@@ -14,11 +14,11 @@ ui<-dashboardPage(
   dashboardBody(
     
       fluidRow(column(12,h3('Here is example of clickable infobox. It uses shinyjs for the onclick event and has been turned into a shiny Module'))),
-      InfoBoxModal_UI("InfoBox"),
+      InfoBoxModal_UI("InfoBox",color='green',icon='car'),
       
       fluidRow(column(12,h3('Another info box we can click on, we can add icons and colors as parameters for the shiny module, but for now its the same'))),
       fluidRow(column(12,h3('But, The nice thing is it is a function that can be used over and over to create more boxes. A nice feature is the function allows for arbitary UI elements to be displayed in the modal. You pass a tagList of the things you want displayed.'))),
-      InfoBoxModal_UI("InfoBox2")
+      InfoBoxModal_UI("InfoBox2",color='blue')
       
       
       
@@ -30,7 +30,7 @@ server<-function(input,output,session){
   
   callModule(InfoBoxModal,"InfoBox",
              #The module can take in a tagList of elements to be displayed when you click the box
-             tagList(fluidRow(column(6,h3('List of Stuff for MOdal')),
+             elements=tagList(fluidRow(column(6,h3('List of Stuff for MOdal')),
                               column(6,h4('More Stuff'))),
                      DT::dataTableOutput('testInfoBox_DT'),
                      hr(),
@@ -40,7 +40,7 @@ server<-function(input,output,session){
   
    callModule(InfoBoxModal,"InfoBox2",
              #The module can take in a tagList of elements to be displayed when you click the box
-             tagList(h2('Hello!')
+             elements=tagList(h2('Hello!')
                     
                      ))
   
